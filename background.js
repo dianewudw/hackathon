5215927af404579ffc884d731c54f69ba9e3b889
Diane Wu <dianewudw@gmail.com>
2019-09-18 20:36:16 -0400

    "commit 3"

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
-// script that runs on page-load in chrome
+// script that runs on installation in chrome
 // https://developer.chrome.com/extensions/getstarted
+// runtime events: https://developer.chrome.com/apps/runtime#event-onStartup
+
+
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is light green.");
-    });
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is light green.");
   });
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
