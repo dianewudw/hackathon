37885c4dd47eaf08782e17a144f741c7a82638d8
Diane Wu <dianewudw@gmail.com>
2019-09-18 21:08:16 -0400

    "commit 9"

--- background.js
+++ background.js
@@ -8,19 +8,13 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("The color is light green.");
   });
 
-  
-chrome.runtime.onStart.addListener(function() {
-    new chrome.declarativeContent.ShowPageAction()
-    // console.log("The color is light green.");
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: <all_urls>},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
-
-  // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  //   chrome.declarativeContent.onPageChanged.addRules([{
-  //     conditions: [new chrome.declarativeContent.PageStateMatcher({
-  //       pageUrl: {hostEquals: 'developer.chrome.com'},
-  //     })
-  //     ],
-  //         actions: [new chrome.declarativeContent.ShowPageAction()]
-  //   }]);
-  // });
 });
